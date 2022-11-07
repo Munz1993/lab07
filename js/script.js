f9592f96dd65b646efffe0e4efbbef9098085b79
Munz1993 <112294417+Munz1993@users.noreply.github.com>
2022-11-06 19:07:33 -0600

    rectified syntax

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 function init(){
   function myEventFunction (){
-   	var a = document.getElementById('entryinput');
-	   var b = "Munawar Ali: ";
-    var c = b+a.value
-	   alert(c);
+    var a = document.getElementById('entryinput');
+    var b = "Munawar Ali: ";
+    var c = b+a.value;
+    alert(c);
   }; 
   var d1 = document.getElementById('entrybutton'); 
   d1.addEventListener('click', myEventFunction);	 
   d1.addEventListener('click', function(){
-	 var d2 = document.getElementById('textoutput')
-	 d2.innerHTML = fc.value;
-  })	;
+    var d2 = document.getElementById('textoutput');
+    d2.innerHTML = fc.value;
+  });
 }
 window.addEventListener('load', init);
