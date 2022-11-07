fb23d8ac4a93de96461a132d49a62e65902d9054
Munz1993 <112294417+Munz1993@users.noreply.github.com>
2022-11-06 19:02:13 -0600

    Final Code for JS

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 function init(){
-//add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
+  function myEventFunction (){
+   	var a = document.getElementById('entryinput');
+	   var b = "Munawar Ali: ";
+    var c = b+a.value
+	   alert(c);
+  }; 
+  var d1 = document.getElementById('entrybutton'); 
+  d1.addEventListener('click', myEventFunction);	 
+  d1.addEventListener('click', function(){
+	 var d2 = document.getElementById('textoutput')
+	 d2.innerHTML = fc.value;
+  })	;
+}
 window.addEventListener('load', init);
